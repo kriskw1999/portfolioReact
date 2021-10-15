e13d19fe87f132bf2af45e5ae37a131729945048
kriskw1999 <kriskw1999@gmail.com>
2021-10-15 16:35:41 +0200

    Reactga configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './styles/mainStyles.css'
 import Post from './components/Post';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    ReactGA.initialize('G-CM05KLPND2');
+    ReactGA.pageview('/');
+  },[])
+  
+
   return (
     <div className='main-div'>
       <Post/>
